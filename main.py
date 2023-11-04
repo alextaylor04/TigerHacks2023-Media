18cb2b39ef31ab1489bd39346cc7521f8d967923
Alex Taylor <at1587kc@gmail.com>
2023-11-03 21:25:57 -0500

    Main Branch Published

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import json
 
 app = Flask(__name__)
 
-x =  '{ "name":"John", "age":30, "city":"New York"}'
+x =  '{"name":"John", "age":30, "city":"New York"}'
 
-# parse x:
 y = json.loads(x)
 
 @app.route('/')
 def test():
-    return y
+    return y 
